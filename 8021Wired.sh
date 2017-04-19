aa01e5eae55ffdebe3f6e6aca3536fe8fe4b6ab8
marktube <liuyanchao99@gmail.com>
2017-04-19 11:57:25 +0800

    first commit

---
+++ 8021Wired.sh
@@ -0,0 +1,3 @@
+#bin/sh
+
+eapolcfg startAuthentication --interface en3 --profileID 15BD2C71-6502-40F6-BA20-0A60F8FAE9D0
---
+++ eapconfig/symbol_scope.h
@@ -0,0 +1,20 @@
+#ifndef _S_SYMBOL_SCOPE_H
+#define _S_SYMBOL_SCOPE_H
+
+#ifndef EXTERN
+#define EXTERNextern
+#endif /* EXTERN */
+
+#ifndef PRIVATE_EXTERN
+#define PRIVATE_EXTERN__private_extern__
+#endif /* PRIVATE_EXTERN */
+
+#ifndef STATIC
+#define STATICstatic
+#endif /* STATIC */
+
+#ifndef INLINE
+#define INLINEstatic __inline__
+#endif /* INLINE */
+
+#endif /* _S_SYMBOL_SCOPE_H */
